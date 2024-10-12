deffd659ab6312a2be22f6b61690b2fa9634f403
niakko <91799924+niiass@users.noreply.github.com>
2024-10-12 19:37:56 +0400

    Delete Lecture 3 directory

--- Lecture 3/N1.py
+++
@@ -1,8 +0,0 @@
-from math import pow
-
-x = float(input("Please enter number x: "))
-y = float(input("Please enter number y: "))
-
-power = pow(x, y)
-
-print(x, " to the power of ", y, " equals to ", power)
--- Lecture 3/N2.py
+++
@@ -1,6 +0,0 @@
-from random import uniform
-
-rand = uniform(0, 100000)
-rounded = round(rand, 1)
-
-print(rand, " rounded is " , rounded)
