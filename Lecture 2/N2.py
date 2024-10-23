4f0a9bde23b0ecba79cedf082775127b180b17fe
niakko <91799924+niiass@users.noreply.github.com>
2024-10-23 21:25:28 +0400

    Delete Lecture 2 directory

--- Lecture 2/N2.py
+++
@@ -1,9 +0,0 @@
-number_one = int(input("შეიყვანეთ პირველი რიცხვი: "))
-number_two = int(input("შეიყვანეთ მეორე რიცხვი: "))
-
-mod = number_one % number_two
-
-if mod == 0:
-    print(number_one, " არის ", number_two, "-ს ჯერადი")
-else:
-    print(number_one, " არ არის ", number_two, "-ს ჯერადი")
